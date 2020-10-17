ec51d5ec2e75fd5afacecf4037a3ba00c56871b8
mr-icon <emmanuelaustin116@gamil.com>
2020-10-16 23:22:55 -0700

    pushing back update

---
+++ modal.js
@@ -0,0 +1,24 @@
+var modal = 
+document.getElementById("myModal");
+
+// var btn =
+// document.getElementById("myBtn");
+var span =
+document.getElementsByClassName("close")[0];
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+span.onclick = function() {
+    modal.style.display = "none";
+}
+function openModal(){
+    modal.style.display = 'block'
+    
+}
+window.onclick = function(event) {
+    console.log(event)
+    if(event.target == modal){
+        modal.style.display = "none"
+    }
+    
+}
